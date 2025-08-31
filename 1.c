17ac711e7e2724f62dfbb8514d694228496b8813
Jheylone <juliocs54@outlook.com>
2025-08-31 19:38:00 -0300

    Primeira versão de teste

---
+++ 1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("I\nn\ni\nc\ni\no\nd\no\np\nr\no\ng\nr\na\nm\na\nF\ni\nm\nd\no\np\nr\no\ng\nr\na\nm\na\n");
+
+    return 0;
+}
---
+++ 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("enter an integer: ");
+    scanf("%d", &number);
+    printf("number: %d\n", number);
+
+    return 0;
+}
