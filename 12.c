99b9e26d0f5288e6ea5adc43b3b4245e098dfd0d
Jheylone <juliocs54@outlook.com>
2025-09-05 01:41:54 -0300

    added 12 and 13

---
+++ 12.c
@@ -0,0 +1,12 @@
+/*write a program that contains any constant of type int. use the const command. print this constant*/
+
+#include <stdio.h>
+
+const int i = 0;
+
+int main()
+{
+    printf("i = %d\n", i);
+
+    return 0;
+}
---
+++ 13.c
@@ -0,0 +1,15 @@
+/*make a program that reads a character of type char and then prints it between double quotes. thus, if the character read is the letter A,
+it should print "A"*/
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("enter a char: ");
+    scanf("%c", &ch);
+    printf("\"%c\"\n", ch);
+
+    return 0;
+}
